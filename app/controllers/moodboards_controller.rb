2197819787c489595af74807b427453532857f26
Daniel Bauer <dan717@gmx.de>
2022-08-30 17:28:58 +0200

    Display on testpage working

--- app/controllers/moodboards_controller.rb
+++ app/controllers/moodboards_controller.rb
@@ -1,5 +1,12 @@
 class MoodboardsController < ApplicationController
   def index
     @moodboards = Moodboard.all
+    @random_sofas_item = Item.where(category: "Sofas").sample
+    @random_chairs_item = Item.where(category: "Chairs").sample
+    @random_rugs_item = Item.where(category: "Rugs").sample
+  end
+
+  def new
+    @random_sofas_item = Item.where(category: "Sofas").sample
   end
 end

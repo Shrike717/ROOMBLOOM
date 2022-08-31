4b78bf137ba0b2e896cb3bf1c0fa5444a64c41b0
Azim Alizada <azim-alizada@gmx.net>
2022-08-31 17:54:20 +0200

    moodboard frontend adjusted but not finished yet

--- app/controllers/moodboards_controller.rb
+++ app/controllers/moodboards_controller.rb
@@ -1,9 +1,9 @@
 class MoodboardsController < ApplicationController
   def index
-    # @moodboards = Moodboard.all
-    # @random_sofas_item = Item.where(category: "Sofas").sample
-    # @random_chairs_item = Item.where(category: "Chairs").sample
-    # @random_rugs_item = Item.where(category: "Rugs").sample
+     @moodboards = Moodboard.all
+     @random_sofas_item = Item.where(category: "Sofas").sample
+     @random_chairs_item = Item.where(category: "Chairs").sample
+     @random_rugs_item = Item.where(category: "Rugs").sample
   end
 
   def show
